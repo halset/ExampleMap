2bb4eb56a16565eb2c9657244f9b12380da9332a
halset <halset@ecc.no>
2013-06-26 14:51:16 +0200

    add route-me

--- ExampleMap/ViewController.m
+++ ExampleMap/ViewController.m
@@ -8,6 +8,10 @@
 
 #import "ViewController.h"
 
+#import "RMWMS.h"
+#import "RMWMSSource.h"
+#import "RMMapView.h"
+
 @interface ViewController ()
 
 @end
@@ -18,6 +22,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // a wms layer tile source
+    RMWMS *topowms = [[RMWMS alloc] init];
+    topowms.urlPrefix = @"http://opencache.statkart.no/gatekeeper/gk/gk.open";
+    topowms.layers = @"topo2";
+    RMWMSSource *topoSource = [[RMWMSSource alloc] init];
+    topoSource.wms = topowms;
+    topoSource.uniqueTilecacheKey = @"abc";
+    
+    // set up a map view
+    RMMapView *mapView = [[RMMapView alloc] initWithFrame:self.view.frame
+                                            andTilesource:topoSource];
+    mapView.centerCoordinate = CLLocationCoordinate2DMake(60.03, 10.19);
+    mapView.zoom = 11.0;
+    mapView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+
+    // add in map view
+    [self.view addSubview:mapView];
 }
 
 - (void)didReceiveMemoryWarning
